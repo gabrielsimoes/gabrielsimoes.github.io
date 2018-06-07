b1e7d50133411e7f395799c2c42ad8f9ef5ed2a5
Gabriel Silva Simões <simoes.sgabriel@gmail.com>
2018-06-07 14:50:41 -0300

    Deploy to master branch using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp');
+var ghpages = require('gh-pages');
 
 gulp.task('html', function(){
   return gulp.src('assets/html/*.html')
-    .pipe(gulp.dest('dist/html'))
+    .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('deploy', function(callback) {
+  ghpages.publish('dist/', {
+      branch: 'master',
+  }, callback);
 });
